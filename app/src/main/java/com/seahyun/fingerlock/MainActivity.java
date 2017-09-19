a37ddc930e037eaeb62aaf97bc76bc462f783f2c
Leesihyun <sihyun0107@github.com>
2017-09-20 00:40:50 +0900

    [fix] screen lock on/off screen button change

--- app/src/main/java/com/seahyun/fingerlock/MainActivity.java
+++ app/src/main/java/com/seahyun/fingerlock/MainActivity.java
@@ -7,29 +7,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Switch;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-    private Button startServiceButton;
-    private Button stopServiceButton;
-    private Button checkAliveButton;
+    //private Button startServiceButton;
+    //private Button stopServiceButton;
+    //private Button checkAliveButton;
+    Switch touch_tab;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startServiceButton = (Button)findViewById(R.id.startService);
+        touch_tab = (Switch)findViewById(R.id.touch_tab);
+        touch_tab.setOnClickListener(this);
+        /*startServiceButton = (Button)findViewById(R.id.startService);
         stopServiceButton = (Button)findViewById(R.id.stopService);
         checkAliveButton = (Button)findViewById(R.id.checkAlive);
 
         startServiceButton.setOnClickListener(this);
         stopServiceButton.setOnClickListener(this);
-        checkAliveButton.setOnClickListener(this);
+        checkAliveButton.setOnClickListener(this);*/
     }
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
+
+        if(touch_tab.isChecked()){
+            startService(new Intent(this, SimpleService.class));
+            touch_tab.setChecked(true);
+        }
+        else{
+            stopService(new Intent(this, SimpleService.class));
+            touch_tab.setChecked(false);
+        }
+        /*switch(v.getId()){
             case R.id.startService:
                 startService(new Intent(this, SimpleService.class));
                 break;
@@ -41,6 +54,6 @@ public class MainActivity extends Activity implements OnClickListener {
             case R.id.checkAlive:
                 sendBroadcast(new Intent("com.androidhuman.action.isAlive"));
                 break;
-        }
+        }*/
     }
 }
